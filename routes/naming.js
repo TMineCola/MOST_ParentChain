4ec5b6f3d0debcd1719ebe64f618f36f82c116d1
Wang Jiun Jie <wang.minecola@gmail.com>
2020-06-11 14:36:38 +0800

    Added search ip for chain id

--- routes/naming.js
+++ routes/naming.js
@@ -4,6 +4,18 @@ const { ContractManager } = require('../lib/contract-manager');
 
 const manager = new ContractManager();
 
+router.get('/:chainId', async (req, res, next) => {
+    let chainId = req.params.chainId;
+
+    try {
+        console.log("[INFO] Search IP for chain " + chainId);
+        return res.status(200).json({ info: "查詢成功", ip: await manager.searchNaming(chainId) }); 
+    } catch (err) {
+        console.log("[ERROR] Registe error. Error: " + err.message);
+        return res.status(500).json({ error: "伺服器發生錯誤" });
+    }
+});
+
 router.post('/', async (req, res, next) => {
     let namingData = req.body;
 
@@ -14,7 +26,7 @@ router.post('/', async (req, res, next) => {
         await manager.registeNaming(namingData.account, namingData.chainId, namingData.ip);
         return res.status(200).json({ info: "註冊成功" });
     } catch (err) {
-        console.log("[ERROR] Registe error. Error: " + err);
+        console.log("[ERROR] Registe error. Error: " + err.message);
         return res.status(500).json({ error: "伺服器發生錯誤" });
     }
 });
