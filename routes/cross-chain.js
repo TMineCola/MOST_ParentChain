5cd002966d235fe366d0a5c1d85993c23b978082
Wang Jiun Jie <wang.minecola@gmail.com>
2020-06-11 14:41:29 +0800

    Sync from child chain

--- routes/cross-chain.js
+++ routes/cross-chain.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { TX_STATE } = require('../model/enum/crosschain-status');
 const { ContractManager } = require('../lib/contract-manager');
+const { TransactionSearcher } = require('../lib/transaction-searcher');
 
 const manager = new ContractManager();
+const txSearch = new TransactionSearcher();
 
 router.get('/:hash', async (req, res, next) => {
     let hash = req.params.hash;
@@ -12,9 +15,17 @@ router.get('/:hash', async (req, res, next) => {
 
         let txRes = await manager.getRecentState(hash);
 
-        return (txRes) ? 
-                res.status(200).json({ info: "查詢成功", status: txRes.words[0], rawResponse: txRes }) :
-                res.status(404).json({ error: "查詢不到該筆狀態" });
+        if (txRes) {
+            let txHash = await manager.getInitTx(hash);
+            let txData = await txSearch.receiveCrossChainTx(txHash);
+            
+            (txData) ?
+            res.status(200).json({ info: "查詢成功", status: txRes.words[0], data: txData }) :
+            res.status(404).json({ error: "查詢不到該筆跨鏈交易原始紀錄" });
+        } else {
+            res.status(404).json({ error: "查詢不到該筆跨鏈交易狀態" });
+        }
+
     } catch (err) {
         console.log("[ERROR] Search state error. Error: " + err.message);
         return res.status(500).json({ error: "伺服器發生錯誤" });
@@ -25,18 +36,25 @@ router.get('/:hash', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
     let txData = req.body;
 
-    if (!txData.from || !txData.to || !txData.data ) res.status(400).json({ error: "缺少資料欄位" });
+    if (!txData.from || !txData.to || !txData.data ) return res.status(400).json({ error: "缺少資料欄位" });
 
     try {
         console.log("[INFO] Creating cross chain transaction from " + txData.from + " to " + txData.to);
 
+        // Create cross chain transaction
         let txRes = await manager.sendTransaction(txData.from, txData.to, "REQUEST", txData.data);
+        // Calculate data hash for search
         let dataHash = await manager.calcHash(txData.from, txData.to, "REQUEST", txData.data);
+        // If cross chain is from relay chain send to us, It's Prepare state
+        if (txData.to === parseInt(process.env.CHAIN_ID)) await manager.changeState(dataHash, TX_STATE.PREPARE);
 
-        res.status(200).json({ info: "跨鏈交易請求已提交", dataHash: dataHash, rawResponse: txRes });
+        // Log transaction to contract
+        await manager.setInitTx(dataHash, txRes.receipt.transactionHash);
+    
+        return res.status(200).json({ info: "跨鏈交易請求已提交", dataHash: dataHash, rawResponse: txRes });
     } catch (err) {
         console.log("[ERROR] Create cross chain transaction error. Error: " + err.message);
-        res.status(500).json({ error: "伺服器發生錯誤" });
+        return res.status(500).json({ error: "伺服器發生錯誤" });
     }
 });
 
@@ -52,7 +70,7 @@ router.patch('/', async (req, res, next) => {
 
         if (currentState) {
             // check if step is correct
-            if (parseInt(currentState) !== parseInt(txData.state) + 1) return res.status(400).json({ error: "跨鏈請求狀態不對稱" });
+            if ((parseInt(currentState) !== parseInt(txData.state) + 1) && !TX_STATE.REVOKE) return res.status(400).json({ error: "跨鏈請求狀態不對稱" });
             // invoke change state
             await manager.changeState(txData.hash, txData.state);
             console.log("[INFO] Change cross chain transaction " + txData.hash + " state to " + txData.state + " success.");
@@ -63,7 +81,7 @@ router.patch('/', async (req, res, next) => {
         }
 
     } catch (err) {
-        console.log("[ERROR] Change cross chain transaction " + txData.hash + " state to " + state + "fail. Error: " + err);
+        console.log("[ERROR] Change cross chain transaction " + txData.hash + " state to " + state + "fail. Error: " + err.message);
         return res.status(500).json({ error: "伺服器發生錯誤" });
     }
 });
